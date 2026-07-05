befcf57ee93f72d68b0ef7d6756108275f10fd9d
lpzneider <lpzneider@gmail.com>
2026-07-05 10:35:51 -0500

    feat: add

---
+++ infrastructure/entry-points/sqs-listener/src/main/java/co/com/nequi/sqs/listener/config/SqsListenerObjectMapperConfig.java
@@ -0,0 +1,16 @@
+package co.com.nequi.sqs.listener.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SqsListenerObjectMapperConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper sqsListenerObjectMapper() {
+        return new ObjectMapper();
+    }
+}
